2ead36fff8d2899d33fadf2466c890473c6a5d55
Anninas <48886970+Anninas@users.noreply.github.com>
2020-04-10 12:23:42 +0200

    Updated Data Augmentation after Videoconference

--- Programm/addSlicingSymbols.py
+++ Programm/addSlicingSymbols.py
@@ -14,11 +14,88 @@ import PIL
 import matplotlib.pyplot as plt
 from keras import layers
 from keras.models import Model
+#import OpenCV as cv
+import random
+
+random.seed()
+
+
+
+
+###START FUNCTION SECTION   
+                
+def StandardAnnotation(img, bound):
+    
+    annot_x1 = int(bound[0])                                     #Get x-coordinate of upper left corner rounded down
+    annot_y1 = int(bound[1])                                     #Get y-coordinate of upper left corner rounded down
+    annot_width = int(np.ceil(bound[2]))                         #Get width rounded up
+    annot_height = int(np.ceil(bound[3]))                        #Get height rounded up
+    annot_x2 = annot_x1 + annot_width                            #Calculate x-coordinate of lower right corner 
+    annot_y2 = annot_y1 + annot_height                           #Calculate y-coordinate of lower right corner
+    
+    return img[annot_y1:annot_y2, annot_x1:annot_x2]
+    
+def OffsetHorizontalAnnotation(img, bound):
+     
+     offset = random.randint(10, 50)
+     
+     annot_x1 = int(bound[0]) + offset                            #Get x-coordinate of upper left corner rounded down
+     annot_y1 = int(bound[1])                                     #Get y-coordinate of upper left corner rounded down
+     annot_width = int(np.ceil(bound[2]))                         #Get width rounded up
+     annot_height = int(np.ceil(bound[3]))                        #Get height rounded up
+     annot_x2 = annot_x1 + annot_width                            #Calculate x-coordinate of lower right corner 
+     annot_y2 = annot_y1 + annot_height                           #Calculate y-coordinate of lower right corner
+     
+     return img[annot_y1:annot_y2, annot_x1:annot_x2]
+        
+def OffsetVerticalAnnotation(img, bound):
+     
+     offset = random.randint(10, 50)
+     
+     annot_x1 = int(bound[0])                                     #Get x-coordinate of upper left corner rounded down
+     annot_y1 = int(bound[1])  + offset                           #Get y-coordinate of upper left corner rounded down
+     annot_width = int(np.ceil(bound[2]))                         #Get width rounded up
+     annot_height = int(np.ceil(bound[3]))                        #Get height rounded up
+     annot_x2 = annot_x1 + annot_width                            #Calculate x-coordinate of lower right corner 
+     annot_y2 = annot_y1 + annot_height                           #Calculate y-coordinate of lower right corner
+     
+     return img[annot_y1:annot_y2, annot_x1:annot_x2]
+ 
+def Offset2dAnnotation(img, bound):
+    
+     offset_x = random.randint(10, 50)
+     offset_y = random.randint(10, 50)
+     
+     annot_x1 = int(bound[0]) + offset_y                          #Get x-coordinate of upper left corner rounded down
+     annot_y1 = int(bound[1]) + offset_x                          #Get y-coordinate of upper left corner rounded down
+     annot_width = int(np.ceil(bound[2]))                         #Get width rounded up
+     annot_height = int(np.ceil(bound[3]))                        #Get height rounded up
+     annot_x2 = annot_x1 + annot_width                            #Calculate x-coordinate of lower right corner 
+     annot_y2 = annot_y1 + annot_height                           #Calculate y-coordinate of lower right corner
+     
+     return img[annot_y1:annot_y2, annot_x1:annot_x2]
+    
+#def RotateAnnotation(img, bound):                                 #Rotation of offset or standard annotation
+    
+    #Ausschnitt, etwas größer als Annotation, drehen
+    #imutils installieren, s. Skype
+    #45°, Vielfache davon
+    
+ #   case = random.randint(0, 3)
+    
+  #  if case == 0:
+        
+    
+###END FUNCTION SECTION
+
+#Skalierung mit opencv.resize, interpolation cubic
+
+
 
 ###START LOADING DATA
 
 #Load json file    
-metadata = json.loads(open("D:/Users/anni/Documents/KooperationsphaseHector/Flurplandaten/flo2plan-ICDAR2019/flo2plan_icdar_instances.json", 'r').read())
+metadata = json.loads(open("C:/Users/annika/CloudStation/KooperationsphaseHector/Flurplandaten/flo2plan-ICDAR2019/flo2plan_icdar_instances.json", 'r').read())
 
 #List for images
 imgs = []
@@ -27,7 +104,7 @@ imgs = []
 imgs_nametoind = {}
 
 #Path for accessing images
-path = "D:/Users/anni/Documents/KooperationsphaseHector/Flurplandaten/flo2plan-ICDAR2019/images/"
+path = "C:/Users/annika/CloudStation/KooperationsphaseHector/Flurplandaten/flo2plan-ICDAR2019/images"
 
 #Counter to get index of image
 count = 0
@@ -35,9 +112,9 @@ count = 0
 #Loop through elements (-->e) in directory
 for e in listdir(path):
     
-    link = join(path, e)                            #Get the element's link
-    curr_img = np.array(PIL.Image.open(link))       #Load image and transform it to numpy array
-    imgs.append(curr_img)                           #Add array-shaped image to list
+    link = join(path, e)                                            #Get the element's link
+    curr_img = np.array(PIL.Image.open(link).convert('L'))         #Load image and transform it to B&W numpy array
+    imgs.append(curr_img)                                           #Add array-shaped image to list
     
     imgs_nametoind[e] = count
     
@@ -50,11 +127,11 @@ imgs_idtoind = {}
 #Loop through all image entries (-->ie) in metatdata
 for ie in metadata['images']:
     
-    if ie['file_name'] in imgs_nametoind:           #Check if image exists in imported image list
+    if ie['file_name'] in imgs_nametoind:                           #Check if image exists in imported image list
         
-        index = imgs_nametoind[ie['file_name']]     #Get the index to the current file name
-        img_id = ie['id']                           #Get the id of this file
-        imgs_idtoind[img_id] = index                #Link id to the index
+        index = imgs_nametoind[ie['file_name']]                     #Get the index to the current file name
+        img_id = ie['id']                                           #Get the id of this file
+        imgs_idtoind[img_id] = index                                #Link id to the index
 
 ###END LOADING DATA
 
@@ -65,37 +142,42 @@ annotations = []
 
 #Loop through all annotations
 for annot in metadata['annotations']:
-    bbox = annot['bbox']                             #Get bounding box
-    img_id = annot['image_id']                       #Get image id
+    bbox = annot['bbox']                                            #Get bounding box
+    img_id = annot['image_id']                                      #Get image id
     
     #Check if the image to the id exists
     if img_id in imgs_idtoind:
         
-        annot_img = imgs[imgs_idtoind[img_id]]       #Get the image
-        annot_x1 = int(bbox[0])                      #Get x-coordinate of upper left corner rounded down
-        annot_y1 = int(bbox[1])                      #Get y-coordinate of upper left corner rounded down
-        annot_width = int(np.ceil(bbox[2]))          #Get width rounded up
-        annot_height = int(np.ceil(bbox[3]))         #Get height rounded up
-        annot_x2 = annot_x1 + annot_width            #Calculate x-coordinate of lower right corner 
-        annot_y2 = annot_y1 + annot_height           #Calculate y-coordinate of lower right corner
+        annot_img = imgs[imgs_idtoind[img_id]]                      #Get the image
         
         #Add current annotation to list
-        annotations.append(annot_img[annot_y1:annot_y2, annot_x1:annot_x2])
+        annotations.append(StandardAnnotation(annot_img, bbox))
+        
+        #Create 20 varied Verions of this Annotation        
+        for i in range(0, 20):
+            
+            augmentation = random.randint(0, 2)
+            
+            if augmentation == 0:                                   #Horizontaler Offset
+                aug_annot = OffsetHorizontalAnnotation(annot_img, bbox)
+                annotations.append(aug_annot)
+            elif augmentation == 1:                                 #Vertikaler Offset
+                aug_annot = OffsetVerticalAnnotation(annot_img, bbox)    
+                annotations.append(aug_annot)
+            elif augmentation == 2:                                 #2D - Offset
+                aug_annot = Offset2dAnnotation(annot_img, bbox)
+                annotations.append(aug_annot)
+            #elif augmentation == 3:
+                
+            #elif augmentation == 4:
+                
+            #else:
+        
+        
 
 ###END PREPROCESSING 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
     
     
     
